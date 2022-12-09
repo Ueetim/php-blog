afc17d7229a20c163589ad67db8d539007274427
uduak <lorduduak@gmail.com>
2022-12-09 23:34:00 +0100

    post class

--- models/Post.php
+++ models/Post.php
@@ -1,6 +1,7 @@
 <?php
 
-class Post {
+class Post
+{
     private $conn;
     private $table = 'posts';
 
@@ -12,4 +13,39 @@ class Post {
     public $body;
     public $author;
     public $created_at;
+
+    // constructor with DB
+    public function __construct($db)
+    {
+        $this->conn = $db;
+    }
+
+    // get posts
+    public function readPosts()
+    {
+        // create query
+        $query = 'SELECT 
+                c.name as category_name,
+                p.id,
+                p.category_id,
+                p.title,
+                p.body,
+                p.author,
+                p.created_at
+            FROM 
+                ' . $this->table . 'p
+            LEFT JOIN 
+                categories c ON p.category_id = c.id
+            ORDER BY
+                p.created_at DESC
+        ';
+
+        // prepare statement
+        $stmt = $this->conn->prepare($query);
+
+        // execute query
+        $stmt->execute();
+
+        return $stmt;
+    }
 }
